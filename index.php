6d051e3148a9a503ee5296943b8c08dceea31ef0
Eduardo Klein Diefenthaler <120498575+eduardo-klein-dev@users.noreply.github.com>
2022-12-19 15:17:36 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barber</title>
+    <link rel="icon" href="img/logo1.webp">
+
+    <!-- Links CSS: -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Links Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Merriweather+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+
+    <header>
+        <div class="container cabecalho">
+            <div class="logo">Barber</div>
+            <div class="menu">
+                <div class="menu-icon"></div>
+                <div class="menu-icon"></div>
+                <div class="menu-icon"></div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section id="banner">
+            <div class="container banner">
+                <div class="content-banner">
+                    <h4>Ouve e barba</h4>
+                    <h1>Bem-vindo à<br>Barbearia</h1>
+                    <button>Serviço de barbeiro</button>
+                </div>
+                <div class="img-logo"><img src="img/logo1.png" alt=""></div>
+            </div>
+        </section>
+
+        <section id="quem-somos">
+            <div class="container">
+                <div class="content-quemsomos">
+                    <div class="content-quemsomos-left">
+                        <h5 class="subtitulo">Bem vindo ao conselho</h5>
+                        <h2 class="titulo">Somos Barber Shop</h2>
+                        <div class="linne"></div>
+                        <p>A barbearia costumava ser maior do que simplesmente uma área para obter um penteado ou corte; era um centro da comunidade onde as pessoas se reuniam para se relacionar, socializar e trocar sugestões. Com o objetivo de trazer essa camaradagem em grande estilo, Barber Shop, uma barbearia e loja com locais na cidade de Nova York.<br><br>Obrigado pelo apoio a Barbearia!</p>
+                        <button>Consulte mais informações</button>
+                    </div>
+                    <div class="content-quemsomos-img">
+                        <img src="img/barbeiro1.png" alt="">
+                    </div>
+                    <div class="content-quemsomos-right">
+                        <h5 class="subtitulo">+90 123 456 32 11</h5>
+                        <div>
+                            <h3>Jornada de<br>trabalho</h3>
+                        </div>
+                        <div class="linne"></div>
+                        <div class="content-quemsomos-right-hors"><b>seg-qui </b>09h00 - 19h00<br><b>sábado </b>09h00 - 19h00</div>
+                        <div class="content-quemsomos-right-text">* Serviços abrangentes de barbeiro * Pacotes de presente * Produtos exclusivos de higiene * Juniper * Coquetéis artesanais * Visa / MasterCard / Amex</div>
+                        <div class="linne"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="galeria-fotos">
+            <h5 class="subtitulo">Galeria de Fotos</h5>
+            <h2 class="titulo">MAN & BOYS FULL SERVICE</h2>
+            <div class="galeria-area">
+                <div class="galeria-area-left p-3">
+                    <div class="img1"><img src="img/c1.png" alt=""></div>
+                    <div><img src="img/c4.png" alt=""></div>
+                </div>
+                <div class="galeria-area-center p-3">
+                    <div class="img2"><img src="img/c2.png" alt=""></div>
+                    <div><img src="img/c5.png" alt=""></div>
+                </div>
+                <div class="galeria-area-right p-3">
+                    <div class="img3"><img src="img/c3.png" alt=""></div>
+                    <div><img src="img/c6.png" alt=""></div>
+                </div>
+            </div>
+        </section>
+
+        <section id="sobre">
+            <div class="container img-fundo-sobre"> <img src="img/logo-preto.png" alt=""></div>
+            <div class="container sobre">
+                <h5 class="subtitulo-sobre">SOBRE A BARBEARIA</h5>
+                <h2 class="titulo-sobre">APROVEITE O SEU CORTE DE CABELO<br>NOVAMENTE</h2>
+                <div class="texto-sobre">
+                    <div class="texto-esquerda">
+                        At The Barbershop A Hair Salon for Men, we are dedicated to providing the best value. We do this by providing high quality salon services for men and boys, a unique customer focus, all in a warm, friendly atmosphere. We have removed almost all of the pain from getting a haircut. Donec vulputate orci lectus, in laoreet massa ornare sed. Quisque lorem magna, euismod sed enim id, fermentum blandit elit. In hac habitasse platea dictumst. Nunc nisi magna, posuere at ex tempor, sollicitudin maximus velit. Pellentesque blandit, ligula sit amet hendrerit lobortis, nisi odio consequat ipsum, in commodo dui orci ullamcorper sem. Cras volutpat et massa ut efficitur. Sed molestie diam hendrerit ultricies elementum. Quisque congue libero felis, eget vehicula nibh ultrices eu. Suspendisse posuere laoreet tincidunt. Etiam vitae cursus eros. In tincidunt vel.
+                    </div>
+                    <div class="texto-direita">
+                        And if that isn't enough, not only will your stylist cut your hair, your stylist will give you a relaxing shampoo and cooling conditioner, a hot soothing towel, trim your eyebrows and mustache. Let us earn your business, and we will do our best to make you want to come back, every time you need a haircut. Nunc eget odio sed eros auctor mattis. Aenean feugiat interdum ex id aliquet. Nullam vitae luctus ante. Donec sit amet odio mi. Fusce ac odio justo. Pellentesque sit amet arcu venenatis, aliquet sapien id, dapibus lacus. Fusce facilisis nibh a mattis tempor. Suspendisse posuere laoreet tincidunt. Etiam vitae cursus eros. In tincidunt vel libero euismod mattis. Suspendisse venenatis convallis vestibulum. Aliquam erat volutpat. Sed molestie, ante et ullamcorper bibendum, orci mi fringilla mi, nec ultrices velit massa non dolor.
+                    </div>
+                </div>
+                <button>Consule mais inormações</button>
+            </div>
+
+        </section>
+
+        <section>
+            <div class="container area5">
+                <div class="area5">
+                    <div class="imagem1"></div>
+                    <div class="imagem2"><img src="img/d2.png" alt=""></div>
+                    <div class="imagem3">
+                        <div class="area5-body">
+                            <h5 class="area5-subtitulo">NÓS NOS ESPECIALIZAMOS</h5>
+                            <h2 class="area5-titulo">BARBAS QUENTES</h2>
+                            <p class="area5-text">Somos especializados em Barbear a Quente e no cuidado com os Pêlos Faciais (barbas), a Straight Razor Shave é o facial para homens. A experiência é definitivamente um verdadeiro mimo. Não há nada como uma toalha quente em seu rosto ou a fragrância de creme de barbear quente para lhe dar aquela sensação revigorante.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="area6">
+            <div class="area6">
+                <div class="area6-body">
+                    <div class="area6-img">
+                        <img src="img/d3.png" alt="">
+                    </div>
+                    <div class="area6-text">
+                        <h5 class="area6-subtitulo">OLHANDO UM LINDO ESTILO</h5>
+                        <h2 class="area6-titulo">QUAIS SÃO OS SEGREDOS<br>DO CORTE DE CABELO?</h2>
+                        <div class="area6-linne"></div>
+                        <p>We are a Traditional Barber Shop, We pride ourselves on Masterful Barber Haircuts and Shaves for Men. Our Barbers are accomplished in their craft, specializing in traditional Barbering. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected materials.</p>
+                        <div class="area6-button"><button>Consulte mais informações</button></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="area7">
+            <div class="section-area7">
+                <h5 class="area7-subtitulo">PODEMOS FAZER SUA VIDA INCRÍVEL</h5>
+                <h2 class="area7-titulo">NOSSOS BARBEIROS</h2>
+                <div class="area7-body">
+                    <div class="area7-barbeiros">
+                        <div class="sliders">
+                            <div class="area7-barbeiro">
+                                <div class="area7-barbeiro-body">
+                                    <div class="area7-barbeiro-foto"><img src="img/barbeiro2.jpeg" alt=""></div>
+                                    <div class="area7-barbeiro-desc">barbeiro</div>
+                                    <div class="area7-barbeiro-name">FRED MORRISON</div>
+                                </div>
+                            </div>
+                            <div class="area7-barbeiro">
+                                <div class="area7-barbeiro-body">
+                                    <div class="area7-barbeiro-foto"><img src="img/barbeiro3.jpeg" alt=""></div>
+                                    <div class="area7-barbeiro-desc">barbeiro</div>
+                                    <div class="area7-barbeiro-name">ADRIAN SCOLD</div>
+                                </div>
+                            </div>
+                            <div class="area7-barbeiro">
+                                <div class="area7-barbeiro-body">
+                                    <div class="area7-barbeiro-foto"><img src="img/barbeiro4.jpeg" alt=""></div>
+                                    <div class="area7-barbeiro-desc">barbeiro</div>
+                                    <div class="area7-barbeiro-name">ELMER BRIGGS</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+
+        <section id="area8">
+            <div class="area8-background">
+                <div class="area8-body">
+                    <i class="fa-solid fa-quote-right fa-2xl"></i>
+                    <h2 class="area8-titulo">Depoimentos</h2>
+                    <p class="area8-desc">Eles realmente entenderam o visual que eu estava procurando e acertaram em cheio!<br>Eu recomendo o Tema Historia de longe o melhor tema que já comprei.</p>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer>
+        <div class="footer-body">
+            <img src="img/logo1.png" alt="">
+            <br>
+            <div class="footer-social">
+                <i class="fa-brands fa-facebook-f fa-lg"></i>
+                <i class="fa-brands fa-twitter fa-lg"></i>
+                <i class="fa-brands fa-instagram fa-lg"></i>
+                <i class="fa-brands fa-linkedin-in fa-lg"></i>
+            </div>
+            <h5>Template gerado por <a href="https://nicepage.com/"><u>Nicepage</u></a></h5>
+            <h5>Código fonte programado por <a href="https://github.com/eduardo-klein-dev"><u>@eduardo-klein-dev</a></u></h5>
+        </div>
+    </footer>
+
+    <!-- Links JS: -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
